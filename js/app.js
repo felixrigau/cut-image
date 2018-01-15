44839024028984a4617350c37cb5246c31ac3610
Felix <felixrigau88@gmail.com>
2018-01-15 16:25:37 +0100

    Capture movement of mouse

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 var app = {
-
+  events:{
+    moveCutSquare:function () {
+      var square = document.querySelector('.cut-square');
+      var clientX = document.querySelector('.clientX');
+      var clientY = document.querySelector('.clientY');
+      square.addEventListener('mousemove',function (e) {
+        if (e.buttons === 1) {
+          console.log(e.buttons);
+          console.log(event.movementX,event.movementY);
+        }
+      });
+    }
+  }
 };
 
 (function () {
-    
+    app.events.moveCutSquare();
 })();
