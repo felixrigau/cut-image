0ebb3b3b059cb1fe2f968ad9e39ff9caf7196287
Felix <felixrigau88@gmail.com>
2018-01-15 16:40:18 +0100

    Move the cutsquare is ready

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ var app = {
       var clientY = document.querySelector('.clientY');
       square.addEventListener('mousemove',function (e) {
         if (e.buttons === 1) {
+          square.style.left=(parseInt(square.style.left)+event.movementX).toString()+'px';
+          square.style.top=(parseInt(square.style.top)+event.movementY).toString()+'px';
+          clientX.innerText = parseInt(clientX.innerText)+event.movementX;
+          clientY.innerText = parseInt(clientY.innerText)+event.movementY;
           console.log(e.buttons);
           console.log(event.movementX,event.movementY);
         }
